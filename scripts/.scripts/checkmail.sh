0d6d1127e4a1c07c5615407ba17a039d44ef69fe
plup <plup@plup.io>
2019-03-23 13:56:41 +0100

    feat: added notmuch config

--- scripts/.scripts/checkmail.sh
+++ scripts/.scripts/checkmail.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+set -e
 
 # check network connection
 wicd-cli -i | grep 'status: Connected' > /dev/null
 if [ $? -eq 0 ]; then
-    exec offlineimap
+    offlineimap
+    notmuch new
+    exit 0
 fi
 echo "No internet connection."
-exit 0
